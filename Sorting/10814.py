8a76d921580370da64a574a6f7fe4d5888fbe20a
10000001a <pgdn06288888@gmail.com>
2020-05-30 01:42:36 +0900

    ADD: Boj 10814(나이순 정렬).

---
+++ Sorting/10814.py
@@ -0,0 +1,18 @@
+import sys
+
+In = sys.stdin.readline
+
+N = int(In())
+
+userList = []
+answer = ''
+
+for i in range(N):
+    a = input().split(' ')
+
+    userList.append(tuple([int(a[0]), a[1]]))
+
+sortedList = sorted(userList, key=lambda x: x[0])
+
+for tupleData in sortedList:
+    print(tupleData[0], tupleData[1])
