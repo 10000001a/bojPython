0b9d868bb45cc57e6627cad8c02e2e0c410d2309
10000001a <pgdn06288888@gmail.com>
2020-06-08 03:54:02 +0900

    ADD: Boj 7490(0 만들기), 10844(쉬운 계단 수).

--- RecursiveCall/7490.py
+++ RecursiveCall/7490.py
@@ -1,9 +1,33 @@
 import sys
 
 In = sys.stdin.readline
+Out = sys.stdout.write
 
-N = int(In()[:-1])
-answer = ''
+testNumber = int(In()[:-1])
+answer = [[] for _ in range(testNumber)]
 
-for _ in range(N):
-    x = int(In()[:-1])
+
+def evaluate(string):
+    return eval(string.replace(' ', ''))
+
+
+def findZero(exp, num, N, caseNumber):
+    if num != N:
+        findZero(exp + '+' + str(num + 1), num + 1, N, caseNumber)
+        findZero(exp + '-' + str(num + 1), num + 1, N, caseNumber)
+        findZero(exp + ' ' + str(num + 1), num + 1, N, caseNumber)
+    else:
+        if evaluate(exp) == 0:
+            global answer
+            answer[caseNumber].append(exp)
+
+
+for i in range(testNumber):
+    N = int(In()[:-1])
+    findZero('1', 1, N, i)
+    answer[i].sort()
+
+for data1 in answer:
+    for data2 in data1:
+        Out(data2+'\n')
+    Out('\n')
